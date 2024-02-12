75fbb1174eec55c9b7445e8ffe0555d1a2e75647
Israel <isltimi1000@gmail.com>
2024-02-12 05:15:17 +0100

    send manual fulfilment

--- medusa-config.js
+++ medusa-config.js
@@ -34,7 +34,7 @@ const DATABASE_URL =
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
-  // `medusa-fulfillment-manual`,
+  `medusa-fulfillment-manual`,
   // `medusa-payment-manual`,
   // {
   //   resolve: `@medusajs/file-local`,
@@ -59,6 +59,22 @@ const plugins = [
       secret_key: process.env.PAYSTACK_SECRET_KEY,
     },
   },
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        // download_file_duration:
+        //   process.env.S3_DOWNLOAD_FILE_DURATION,
+        // prefix: process.env.S3_PREFIX,
+    },
+  },
+
 //   {
 //   resolve: `medusa-plugin-algolia`,
 //   options: {

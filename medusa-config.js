9ccc044bf783b290a8d1ce1b3eb79abcf07922b0
Israel <isltimi1000@gmail.com>
2024-02-21 04:28:17 +0100

    installed it & hopeful

--- medusa-config.js
+++ medusa-config.js
@@ -34,6 +34,21 @@ const DATABASE_URL =
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        download_file_duration:
+            process.env.S3_DOWNLOAD_FILE_DURATION,
+            prefix: process.env.S3_PREFIX,
+    },
+  },
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
   {
@@ -59,22 +74,6 @@ const plugins = [
       secret_key: process.env.PAYSTACK_SECRET_KEY,
     },
   },
-  // {
-  //   resolve: `medusa-file-s3`,
-  //   options: {
-  //       s3_url: process.env.S3_URL,
-  //       bucket: process.env.S3_BUCKET,
-  //       region: process.env.S3_REGION,
-  //       access_key_id: process.env.S3_ACCESS_KEY_ID,
-  //       secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-  //       cache_control: process.env.S3_CACHE_CONTROL,
-  //       // optional
-  //       download_file_duration:
-  //           process.env.S3_DOWNLOAD_FILE_DURATION,
-  //           prefix: process.env.S3_PREFIX,
-  //   },
-  // },
-
 // {
 //     resolve: `medusa-plugin-algolia`,
 //     options: {
